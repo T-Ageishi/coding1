3c9c599ef82df91ce1dec037988a5614badeb0c9
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-09-22 15:40:07 +0900

    fix: アイコンコンポーネントの色をダークモードに適応できるように調整

--- src/components/atoms/icon/icon.tsx
+++ src/components/atoms/icon/icon.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 import { Props } from "./types.ts";
+import styles from "./styles.module.css";
 
 /**
  * アイコン
  */
-export const Icon: FC<Props> = ({ icon }) => {
-  return <span className="material-symbols-outlined">{icon}</span>;
+export const Icon: FC<Props> = ({ icon, className }) => {
+  return (
+    <span
+      className={`material-symbols-outlined ${styles["material-symbols-outlined"]} ${className ? className : ""}`}
+    >
+      {icon}
+    </span>
+  );
 };
--- src/components/atoms/icon/types.ts
+++ src/components/atoms/icon/types.ts
@@ -1,3 +1,4 @@
 export type Props = {
   icon: string;
+  className?: string;
 };
