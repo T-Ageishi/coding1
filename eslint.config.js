df86160554f96b6df0360db648ee24e1c27c9e72
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-09-23 12:16:08 +0900

    chore: ESLintの設定を更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,27 @@
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import storybook from "eslint-plugin-storybook";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import typescriptParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        parser: "@typescript-eslint/parser",
-      },
+      parser: typescriptParser,
     },
+    files: ["**/*.ts", "**/*.tsx"],
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      storybook: storybook,
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+      "@next/next": nextPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-
-      //ここより上に記載する
-      ...eslintConfigPrettier.rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@next/next/no-img-element": "error",
     },
-  }
-);
+  },
+  {
+    ignores: ["./.next/*"],
+  },
+];
